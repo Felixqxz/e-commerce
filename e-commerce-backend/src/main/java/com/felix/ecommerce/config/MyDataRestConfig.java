e1a132de972b0d0668463c8173360b7ab9bcec6a
Xingzhi Qian <40925246+Felixqxz@users.noreply.github.com>
2022-07-27 00:01:30 +0100

    Add Entity and make database read-only

---
+++ e-commerce-backend/src/main/java/com/felix/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,33 @@
+package com.felix.ecommerce.config;
+
+import com.felix.ecommerce.entity.Product;
+import com.felix.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+/***
+ * Make databases read-only
+ */
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] unsupportedActions = {HttpMethod.DELETE, HttpMethod.PUT, HttpMethod.POST};
+
+        // disable HTTP methods for Product
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+
+        // disable HTTP methods for ProductCategory
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+    }
+}
